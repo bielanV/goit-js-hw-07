92a41a390f8b1611fdd350adf92f08d2a50fa350
Vladyslav Bielan <56whatislove@gmail.com>
2024-08-13 21:24:59 +0300

    finish

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnEl = document.querySelector('.change-color');
+const bodyColor = document.querySelector('body')
+const spanColor = document.querySelector('.color')
+
+const onBtnClick = () => { 
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  const newColor = getRandomHexColor();
+  spanColor.textContent = newColor;
+};
+
+btnEl.addEventListener('click', onBtnClick);
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,48 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+        const controlsEl = document.getElementById('controls');
+        const inputEl = controlsEl.querySelector('input');
+        const createBtnEl = controlsEl.querySelector('[data-create]');
+        const destroyBtnEl = controlsEl.querySelector('[data-destroy]');
+        const boxesEl = document.getElementById('boxes');
+
+        
+        function createBoxes(amount) {
+            
+            destroyBoxes();
+
+            const elements = [];
+            let size = 30;
+
+            for (let i = 0; i < amount; i++) {
+                const div = document.createElement('div');
+                div.style.width = `${size}px`;
+                div.style.height = `${size}px`;
+                div.style.backgroundColor = getRandomHexColor();
+                size += 10;
+                elements.push(div);
+            }
+
+            boxesEl.append(...elements);
+        }
+
+        
+        function destroyBoxes() {
+            boxesEl.innerHTML = '';
+        }
+
+        
+        createBtnEl.addEventListener('click', () => {
+            const amount = Number(inputEl.value);
+
+            if (amount >= 1 && amount <= 100) {
+                createBoxes(amount);
+            }
+
+            inputEl.value = ''; 
+        });
+
+        
+        destroyBtnEl.addEventListener('click', destroyBoxes);
